ec4d0c8ee3e3b6e6a00c306d6a4221cacd12d74f
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-17 14:25:20 +0900

    spring boot initial project

---
+++ src/main/java/com/ZandhiDokkie/minuet/MinuetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ZandhiDokkie.minuet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ZandhiDokkie/minuet/MinuetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZandhiDokkie.minuet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
